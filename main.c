f903c593bfb5ec046b29cae766d68329b3426aa3
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-10 19:20:53 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 Ogni riga contiene i campi separati da virgole, per cui può essere comodo creare una funzione split
 che dalla riga letta ritorni la struttura valorizzata.*/
 
-#define NGIOCHI 16599
-#define DIM 1024
+#define NGIOCHI 16599 //numero di giochi
+#define DIM 1024      //caratteri per riga
+
+typedef char Stringa[DIM];
 
 typedef struct{
     int rank;
@@ -26,46 +28,39 @@ typedef struct{
 
 int main()
 {
-    //Videogioco a[NGIOCHI];
-    //Videogioco *v = a;
     Videogioco *v;
-    //a = (int*)malloc(n * sizeof(int) );
-    v = (Videogioco*)malloc(NGIOCHI * sizeof (Videogioco));
+    Stringa riga;
 
-    char riga[DIM];
+    v = (Videogioco*)malloc(NGIOCHI * sizeof (Videogioco));
 
     FILE *fp;
     fp = fopen("..\\vgsales.csv", "r");
-
 
     //controlla se file esiste
     if(fp == NULL){
         printf("il file non esiste.\n");
     }else{
-        //printf("hey");
         int k = 0;
+
         while (!feof(fp)){
             fgets(riga, DIM, fp);
+            //conto lunghezza riga e creo array della 1 riga con malloc
+            //dopo 1° giro faccio realloc
 
             (*(v + k)).rank = atoi(strtok(riga, ","));
-
             (*(v + k)).name = strtok(NULL, ",");
-
             (*(v + k)).platform = strtok(NULL, ",");
-
             (*(v + k)).year = atoi(strtok(NULL, ","));
-
             (*(v + k)).genre = strtok(NULL, ",");
-
             (*(v + k)).publisher = strtok(NULL, ",");
-
             (*(v + k)).NASales = atof(strtok(NULL, ","));
             (*(v + k)).EUSales = atof(strtok(NULL, ","));
             (*(v + k)).JPSales = atof(strtok(NULL, ","));
             (*(v + k)).otherSales = atof(strtok(NULL, ","));
             (*(v + k)).globalSales = atof(strtok(NULL, ","));
 
-            printf("\n %d, %s, %s, %d, %s, %s, %.2f, %.2f, %.2f, %.2f, %.2f \n", v[k].rank, v[k].name, v[k].platform, v[k].year, v[k].genre, v[k].publisher, v[k].NASales, v[k].EUSales, v[k].JPSales, v[k].otherSales, v[k].globalSales);
+            printf("\n %d, %s, %s, %d, %s, %s, %.2f, %.2f, %.2f, %.2f, %.2f \n", (*(v + k)).rank, (*(v + k)).name, (*(v + k)).platform, (*(v + k)).year,
+                   (*(v + k)).genre, (*(v + k)).publisher, (*(v + k)).NASales, (*(v + k)).EUSales, (*(v + k)).JPSales, (*(v + k)).otherSales, (*(v + k)).globalSales);
 
             k++;
 
@@ -73,7 +68,7 @@ int main()
 
     }
 
-
+    free(v);
     fclose(fp);
     return 0;
 }
