bcf5a463e5a834bb8e89e0082b03195ee3cc9434
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-26 01:35:36 +0200

    Add files via upload

--- sintesi.c
+++ sintesi.c
@@ -9,42 +9,58 @@
  */
 
 #define LUNG 1024 //max caratteri per riga
-#define DIM 4 //max persone
-
-typedef struct {
-    int anno;
-    int mese;
-    int giorno;
-}Nascita;
+#define DIM 10 //max persone
 
 typedef struct {
     char* cognome;
     char* nome;
-    Nascita n;
+    int data;
 }Persone;
 
-void letturaDaFile(Persone *p, char* riga){
+int contaRighe(){
+    FILE* fp;
+    int righe = 0;
+    char c[LUNG];
+    char* riga = c;
+
+    fp = fopen("..\\file.csv", "r");
+
+    //test sulla corretta apertura del file
+    if(fp != NULL){
+        //scorro il file con FGETS
+        while(fgets(riga, LUNG, fp))
+        {
+            righe++;
+        }
+    }else
+        printf("Il file non esiste.\n");
+
+    fclose(fp);
+
+    return righe;
+}
+
+void letturaDaFile(Persone *p, int dim){
     FILE *fp = fopen("..\\file.csv", "r");
+    char c[LUNG];
+    char* riga = c;
+    Persone* posizione = p;  //salva la posizione iniziale del puntatore
 
     if(fp == NULL){
         printf("il file non esiste.\n");
     } else{
-        int k = 0;
-
-        p = (Persone*) malloc(DIM * sizeof (Persone));
         while (!feof(fp)){
             fgets(riga, LUNG, fp);
 
-            (*(p + k)).cognome = strtok(riga, ",");
-            (*(p + k)).nome = strtok(NULL, ",");
-            (*(p + k)).n.anno = atoi(strtok(NULL, ","));
-            (*(p + k)).n.mese = atoi(strtok(NULL, ","));
-            (*(p + k)).n.giorno = atoi(strtok(NULL, ","));
+            p->cognome = strdup(strtok(riga, ","));
+            p->nome = strdup(strtok(NULL, ","));
+            p->data = atoi(strtok(NULL, ","));
 
-            printf("\ncognome: %s\nnome: %s\ndata di nascita: %d-%d-%d\n", (*(p + k)).cognome, (*(p + k)).nome, (*(p + k)).n.anno,
-                   (*(p + k)).n.mese, (*(p + k)).n.giorno);
-            k++;
+            printf("\ncognome: %s\nnome: %s\ndata di nascita: %d\n", p->cognome, p->nome, p->data);
+            p++;
         }
+        p = posizione;
+
     }
 
 }
@@ -58,33 +74,46 @@ void scambio(Persone *x, Persone *y){
 }
 
 void bubbleSort(Persone *p, int dim){
+    Persone* posizione = p;
 
     for(int k = 0; k < dim; k++){
         for(int j = k + 1; j < dim; j++){
-            if((p + k)->n.anno < (p + j)->n.anno){
-                scambio((p + k), (p + j));
+            if(p->data < (p + 1)->data){
+                scambio(p, (p + 1));
             }
+            p++;
         }
     }
+    p = posizione;
 }
-
 
 void visualizza(Persone *p, int dim){
-    for(int k = 0; k < dim; k++){
-        printf("\nnome: %s \ncognome: %s \ndata di nascita: %d-%d-%d\n\n", (*(p + k)).cognome, (*(p + k)).nome, (*(p + k)).n.anno,
-               (*(p + k)).n.mese, (*(p + k)).n.giorno);
+    Persone* posizione = p;
+
+    for(p = posizione; p < posizione + dim; p++){
+        printf("\nnome: %s \ncognome: %s \ndata di nascita: %d\n\n", p->cognome, p->nome, p->data);
     }
+    p = posizione;
 }
 
-int main() {
-    //Persone *p;
-    Persone p[DIM];
-    char riga[LUNG];
 
-    letturaDaFile(&p, &riga);
-    bubbleSort(&p, DIM);
-    visualizza(&p, DIM);
-    //bubbleSort(&p, DIM);
+
+int main() {
+    Persone *p;
+    int righe;
+
+    //conta le righe
+    righe = contaRighe();
+    //printf("le righe sono %d", righe);
+
+    //crea vettore con malloc
+    p = (Persone*)malloc(righe * sizeof (Persone));
+    letturaDaFile(p, righe);
+
+    //ordina e stampa
+    bubbleSort(p, righe);
+    printf("\npersone ordinate per data di nascita: \n\n");
+    visualizza(p, righe);
 
     return 0;
 }
